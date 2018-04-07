5b503922e356cc2f275b8d2e93de8f69b7fde35f
Vitor Araujo <vitorgamer58@outlook.com>
2018-04-07 16:56:28 -0300

    Update CoreRpcServerCommandsDefinitions.h

--- src/Rpc/CoreRpcServerCommandsDefinitions.h
+++ src/Rpc/CoreRpcServerCommandsDefinitions.h
@@ -1,4 +1,6 @@
-// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2012-2018, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2016, The Forknote developers
+// Copyright (c) 2016, The Karbowanec developers
 //
 // This file is part of Bytecoin.
 //
